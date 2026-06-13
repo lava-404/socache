cd78a3803f5b17c4b8691f69eadf2ae03c0384c5
lavanya <lavanyaropt@gmail.com>
2026-06-13 17:10:51 +0530

    feat: added cache

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use std::sync::{
     atomic::{AtomicUsize, Ordering},
     Arc,
 };
+use moka::future::Cache;
+use std::time::Duration;
 
 const NODES: [&str; 5] = [
     "https://mainnet.helius-rpc.com/?api-key=ffe3568c-a4ff-4b2f-a2f5-53d891278489", // helius
@@ -18,11 +20,66 @@ const NODES: [&str; 5] = [
     "https://rpc.ankr.com/solana_devnet/2f5a4dc8e43c3446315186397fd4f9200c32075b310dd7e31acad693f5938dd8",  // ankr
     "https://api.mainnet-beta.solana.com",                                        // mainnet-beta
 ];
+const READ_METHODS: [&str; 43] = [
+    // Cluster and Network Information
+    "getClusterNodes",
+    "getEpochInfo",
+    "getEpochSchedule",
+    "getGenesisHash",
+    "getHealth",
+    "getHighestSnapshotSlot",
+    "getIdentity",
+    "getLeaderSchedule",
+    "getMaxRetransmitSlot",
+    "getMaxShredInsertSlot",
+    "getSlot",
+    "getSlotLeader",
+    "getSlotLeaders",
+    "getVersion",
+    "getVoteAccounts",
+
+    // Blockchain State and Blocks
+    "getBlock",
+    "getBlocks",
+    "getBlockHeight",
+    "getBlockTime",
+    "getBlockCommitment",
+    "getFirstAvailableBlock",
+    "getLatestBlockhash",
+    "isBlockhashValid",
+
+    // Account and Token Data
+    "getAccountInfo",
+    "getBalance",
+    "getMultipleAccounts",
+    "getTokenAccountsByDelegate",
+    "getTokenAccountsByOwner",
+    "getTokenLargestAccounts",
+    "getStakeActivation",
+
+    // Transaction Queries and History
+    "getSignaturesForAddress",
+    "getSignatureStatuses",
+    "getTransaction",
+    "getTransactionCount",
+    "getRecentPerformanceSamples",
+
+    // Economic and Fee Data
+    "getInflationGovernor",
+    "getInflationRate",
+    "getInflationReward",
+    "getStakeMinimumDelegation",
+    "getSupply",
+    "getFeeForMessage",
+    "getRecentPrioritizationFees",
+    "getMinimumBalanceForRentExemption",
+];
 
 // Define our shared application state
 struct AppState {
     current_node_index: AtomicUsize,
     http_client: Client,
+    cache: Cache<String, Value>
 }
 
 #[tokio::main]
@@ -31,7 +88,9 @@ async fn main() {
     let shared_state = Arc::new(AppState {
         current_node_index: AtomicUsize::new(0),
         http_client: Client::new(), // Reusing the client is much faster
+        cache: Cache::builder().time_to_live(Duration::from_secs(2)).build()
     });
+   
 
     let app = Router::new()
         .route("/", get(|| async { "Hello World" }))
@@ -48,15 +107,40 @@ async fn handle_post_with_round_robin(
     Json(payload): Json<Value>,
 ) -> Result<Json<Value>, StatusCode> {
     let num_nodes = NODES.len();
-    
     // Atomically get the current index and increment it for the next request
     // Ordering::Relaxed is perfectly fine here since we just need a counter
     let start_index = state.current_node_index.fetch_add(1, Ordering::Relaxed) % num_nodes;
 
+    let method = payload["method"]
+    .as_str()
+    .unwrap_or("");
+
+    let is_read = READ_METHODS.contains(&method);
+    let cache_key = format!(
+        "{}:{}",
+        payload["method"],
+        payload["params"]
+    );
+
+    let hash = blake3::hash(cache_key.as_bytes());
+
+    let key = hash.to_hex().to_string();
+
+     
+    if is_read {
+        if let Some(cached) = state.cache.get(&key).await {
+            println!("Cache hit");
+            return Ok(Json(cached));
+        }
+    }
+    
     // Loop through nodes starting from 'start_index', trying each one up to 'num_nodes' times
     for offset in 0..num_nodes {
         let current_idx = (start_index + offset) % num_nodes;
         let url = NODES[current_idx];
+
+
+     
 
         // Send the request using our shared client
         let response_result = state
@@ -72,9 +156,16 @@ async fn handle_post_with_round_robin(
 
                 // If the request was successful, return the body immediately
                 if status.is_success() {
-                    if let Ok(body) = response.json::<Value>().await {
-                        return Ok(Json(body));
-                    }
+                    
+                     
+                        if let Ok(body) = response.json::<Value>().await {
+                            if is_read {
+                                state.cache.insert(key.clone(), body.clone()).await;
+                            }
+                        
+                            return Ok(Json(body));
+                        }
+                    
                 } 
                 // Catch 429 Too Many Requests OR 5xx Server Errors
                 else if status == StatusCode::TOO_MANY_REQUESTS || status.is_server_error() {
